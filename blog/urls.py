bb9f7257f24a7204e600aec1a4a0f9e4e5c4d6ec
Yang Dai <yangdai713@aim.com>
2018-08-06 17:08:12 -0400

    blog app works now with minimal functionality

--- blog/urls.py
+++ blog/urls.py
@@ -5,4 +5,5 @@ app_name = 'blog'
 
 urlpatterns = [
     path(r'', views.index, name='index'),
+    path('<int:post_id>', views.post, name='post'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -6,3 +6,9 @@ def index(request):
     posts = Post.objects.order_by('date_added')
     context = {'posts': posts}
     return render(request, 'blog/index.html', context)
+
+
+def post(request, post_id):
+    post = Post.objects.get(id=post_id)
+    context = { 'post': post }
+    return render(request, 'blog/post.html', context)
