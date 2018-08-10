a66688c0d4ddd9a92503ef712555d3aa5c6e427e
Yang Dai <yangdai713@aim.com>
2018-08-10 15:45:54 -0400

    add comment model

--- blog/models.py
+++ blog/models.py
@@ -33,3 +33,20 @@ class Post(models.Model):
     # Admin view on site.
     def get_absolute_url(self):
         return reverse('blog:post_detail', kwargs={'slug': self.slug})
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(
+        'blog.Post', on_delete=models.CASCADE, related_name='comments'
+    )
+    author = models.CharField(max_length=200)
+    text = models.TextField()
+    created_date = models.DateTimeField(default=timezone.now)
+    approved_comment = models.BooleanField(default=False)
+
+    def approve(self):
+        self.approved_comment = True
+        self.save()
+
+    def __str__(self):
+        return self.text
